c5b8ebb637de303fe70500e1091eaf117d987441
ShadowDragon5 <lauriuskas@gmail.com>
2017-03-29 20:40:58 +0300

    Added DoNotGoWhereGhostIs gene

--- Pacman DeepMind/Game.cs
+++ Pacman DeepMind/Game.cs
@@ -81,7 +81,15 @@ namespace Pacman_DeepMind
                 //SetGhostData(ghost.getX(), ghost.getY());
                 //level.Draw();
 
-                PacManEnvironment pe = new PacManEnvironment(level._board, pacman.getX(), pacman.getY(), pacManPrevX, pacManPrevY);       
+                PacManEnvironment pe = new PacManEnvironment(
+					level._board,
+					pacman.getX(),
+					pacman.getY(),
+					pacManPrevX,
+					pacManPrevY,
+					ghost.getX(),
+					ghost.getY()
+					);       
                 Directions? directions = pgp.GetMoveDirection(pe);
 
                 pacManPrevX = pacman.getX();
@@ -140,7 +148,7 @@ namespace Pacman_DeepMind
 
                 _turnCounter++;
 
-                level.Draw();
+                //level.Draw();
                 //Thread.Sleep(100);
 
                 if(_score == _maxScore)
